ddd578d1a9c98d9ab3de85ca2a35c9e730f87379
Dominik Feret <dominik.feret03@gmail.com>
2024-07-26 15:37:41 +0200

    fix userSignupFrom tags syntax

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -101,10 +101,10 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 }
 
 type userSignupForm struct {
-	Name                string `form: "name"`
-	Email               string `form: "email"`
-	Password            string `form: "password"`
-	validator.Validator `form: "-"`
+	Name                string `form:"name"`
+	Email               string `form:"email"`
+	Password            string `form:"password"`
+	validator.Validator `form:"-"`
 }
 
 func (app *application) userSignup(w http.ResponseWriter, r *http.Request) {
