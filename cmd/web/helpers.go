6abe03f68f5d26fb808f55d77c3fbc554ef41d44
Dominik Feret <dominik.feret03@gmail.com>
2024-07-28 17:56:43 +0200

    chore: Add CSRF protection to forms

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/go-playground/form/v4"
+	"github.com/justinas/nosurf"
 )
 
 // Respond with a server error using the provided error message.
@@ -55,6 +56,7 @@ func (app *application) newTemplateData(r *http.Request) templateData {
 		CurrentYear:     time.Now().Year(),
 		Flash:           app.sessionManager.PopString(r.Context(), "flash"),
 		IsAuthenticated: app.isAuthenticated(r),
+		CSRFToken:       nosurf.Token(r),
 	}
 }
 
