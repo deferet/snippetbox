c482f5293269f1bb24fb6cdd5cb3bea32f85d49a
Dominik Feret <dominik.feret03@gmail.com>
2024-07-29 12:52:35 +0200

    update static file serving to use embedded file system

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,24 +2,16 @@ package main
 
 import (
 	"net/http"
-	"path/filepath"
 
+	"github.com/deferet/snippetbox/ui"
 	"github.com/justinas/alice"
 )
-
-// The neuteredFileSystem struct disables directory listing
-// when requesting static files.
-type neuteredFileSystem struct {
-	fs http.FileSystem
-}
 
 // The routes() method returns a servemux containing the application routes.
 func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
-	fileServer := http.FileServer(neuteredFileSystem{http.Dir("./ui/static")})
-	mux.Handle("GET /static", http.NotFoundHandler())
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+	mux.Handle("GET /static/", http.FileServerFS(ui.Files))
 
 	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf, app.authenticate)
 
@@ -39,29 +31,4 @@ func (app *application) routes() http.Handler {
 	standard := alice.New(app.recoverPanic, app.logRequest, commonHeader)
 
 	return standard.Then(mux)
-}
-
-func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
-	f, err := nfs.fs.Open(path)
-	if err != nil {
-		return nil, err
-	}
-
-	s, err := f.Stat()
-	if err != nil {
-		return nil, err
-	}
-	if s.IsDir() {
-		index := filepath.Join(path, "index.html")
-		if _, err := nfs.fs.Open(index); err != nil {
-			closeErr := f.Close()
-			if closeErr != nil {
-				return nil, closeErr
-			}
-
-			return nil, err
-		}
-	}
-
-	return f, nil
 }
